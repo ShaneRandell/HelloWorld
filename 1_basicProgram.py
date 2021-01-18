9ce29cb425a7f36991887a31256367d822d20590
ShaneRandell <77622184+ShaneRandell@users.noreply.github.com>
2021-01-18 10:20:50 -0400

    Add files via upload
    LAB 2 files

---
+++ 1_basicProgram.py
@@ -0,0 +1,29 @@
+"""/********************************
+This is the first part of the training course of Python
+Instructor: Richardo Bautista Quintero
+Date: 2021-01-18
+NSCC
+Ivany Campus
+/***********************************"""
+
+"""
+We will Learn about the following topics:
+
+1) Types of Numbers in Pythons
+2) Basic Arithmetic
+3) Difference between classic division and floor division
+4) Object Assignment in Python
+
+"""
+
+def program_1():
+    a = 2
+    f = 2e4
+
+    print(a)
+    print("is an integer")
+    print(f)
+    print("is an floating point number")
+
+if __name__ == '__main__':
+    program_1()
